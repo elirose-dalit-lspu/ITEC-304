d8753047b559f23995c7829ea2d27320ef602e90
elirose-dalit-lspu <101610497+elirose-dalit-lspu@users.noreply.github.com>
2022-09-14 09:56:35 +0800

    Update flask.py

--- Activity3/flask.py
+++ Activity3/flask.py
@@ -4,15 +4,35 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-name = request.args['name']
+def home():
+return """
+<html>
+<body>
+<h2>Welcome to the greeter</h2>
+<form action="/greet">
+   What is your name? <input type='text' name='username'><br>
+   What is your favorite food? <input type='text' name='favefood'><br>
+   <input type='submit' value='Continue'><br>
+</form>
+</body>
+</html>
+"""
+
+@app.route('/')
+def greet():
+  username = request.args.get('username', 'World')
+  favefood = request.args['favefood']
+  if favefood == '':
+    msg = 'You did nit tell me your favorite food.'
+else:
+  msg = 'I like' + favefood + ', too.'
+  
 return """
 <html><body>
-<h1>Hello, {0}!</h1>
-The time is {1}.
+<h2>Hello, {0}!</h2>
+ {1}
 </body></html>
-""".format(
-name, str(datetime.now()))
+""".format(username, msg)
 
 
 app.run(host="localhost", debug=True)
