4dd9f33b14e764c681db9b6a998c7b6e2ee8be3a
elirose-dalit-lspu <101610497+elirose-dalit-lspu@users.noreply.github.com>
2022-10-10 22:07:15 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return render_template('index2.html')
+
+app.run(host="localhost", debug=True)
