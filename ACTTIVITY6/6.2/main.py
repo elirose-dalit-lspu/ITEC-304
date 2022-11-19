8d700e0ebd9ba4b1c34b1e4c62a39a5c6cfc495b
elirose-dalit-lspu <101610497+elirose-dalit-lspu@users.noreply.github.com>
2022-11-19 23:37:30 +0800

    Update main.py

--- ACTTIVITY6/6.2/main.py
+++ ACTTIVITY6/6.2/main.py
@@ -1,1 +1,36 @@
-scvgvcbbc
+from kivy.app import App
+from kivy.lang import Builder
+
+root = Builder.load_string('''
+FloatLayout:
+    canvas.before:
+        Color:
+            rgba: 0, 0, 255, 0.3
+        Rectangle:
+            # self here refers to the widget i.e FloatLayout
+            pos: self.pos
+            size: self.size
+    Button:
+        text: 'Hello World!!'
+        size_hint: .5, .5
+        pos_hint: {'center_x':.5, 'center_y': .5}
+''')
+
+
+class FloatLayout(App):
+
+    def build(self):
+        return root
+
+if __name__ == '__main__':
+    FloatLayout().run()
+
+
+
+
+
+
+
+
+
+
